b4ce346ed189064dbbda0b79e86b1f21ee16d344
shangguan <shangguan@mpe.mpg.de>
2022-04-18 05:26:55 +0200

    continue the reduction if one target fails

--- bin/run_sofi_jitter.py
+++ bin/run_sofi_jitter.py
@@ -42,25 +42,28 @@ for k_fltr in sciDict:
             print('{}\n  File exists, skip the reduction...\n'.format(combined_name))
             continue
         
-        process = subprocess.Popen([esorex, recipes, log, 'sofi_spc_jitter', scisof], 
-                                   stdout=subprocess.PIPE,
-                                   universal_newlines=True)
-        
-        while True:
-            output = process.stdout.readline()
-            print(output.strip())
-            # Do something else
-            return_code = process.poll()
-            if return_code is not None:
-                print('RETURN CODE', return_code)
-                # Process has finished, read rest of the output 
-                for output in process.stdout.readlines():
-                    print(output.strip())
-                break
-        
-        process = subprocess.Popen(['mv', '{0}/sofi_spc_jitter_combined.fits'.format(work_path), combined_name],
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
+        try:
+            process = subprocess.Popen([esorex, recipes, log, 'sofi_spc_jitter', scisof], 
+                                       stdout=subprocess.PIPE,
+                                       universal_newlines=True)
+            
+            while True:
+                output = process.stdout.readline()
+                print(output.strip())
+                # Do something else
+                return_code = process.poll()
+                if return_code is not None:
+                    print('RETURN CODE', return_code)
+                    # Process has finished, read rest of the output 
+                    for output in process.stdout.readlines():
+                        print(output.strip())
+                    break
+            
+            process = subprocess.Popen(['mv', '{0}/sofi_spc_jitter_combined.fits'.format(work_path), combined_name],
+                         stdout=subprocess.PIPE, 
+                         stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+        except Exception as e:
+            print('Skip this target because the of the following error:\n {}'.format(e))
 
 os.system('mv {0}/*.paf {0}/{1}'.format(work_path, reduced_dir))
